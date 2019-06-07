97813cc749c3bfefc3e466ad42410791917566bc
rahulgul8 <rahulgul8@gmail.com>
2019-06-07 10:08:17 +0530

    1 file modified

--- component/Option.js
+++ component/Option.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Option extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSelected: false
+    };
+  }
   render() {
-    return <label className="optionLabel" ><input type="radio" name="test" value="small"></input><img src={this.props.url}></img><br />
-      {this.props.text}
-    </label>
+    return (
+      <label className="optionLabel" >
+        <input type="radio" name="test" value={this.props.text} onChange={(event) => {
+          console.log(event.target.checked)
+          this.setState({ isSelected: this.checked })
+        }} />
+        <img src={this.props.url} />
+        <br />
+        {this.props.text}
+        {this.state.isSelected}
+      </label>)
   }
 }
 
